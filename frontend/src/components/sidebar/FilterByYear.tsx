cb21d5230689b883c3e398ab39bb554741c72ab6
francinv <francinv@stud.ntnu.no>
2021-10-22 20:41:52 +0200

    (#11) completed filter for year

--- frontend/src/components/sidebar/FilterByYear.tsx
+++ frontend/src/components/sidebar/FilterByYear.tsx
@@ -1,14 +1,16 @@
-import Box from "@mui/material/Box";
 import { FunctionComponent, useState } from "react";
 import TextField from '@mui/material/TextField';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DatePicker from '@mui/lab/DatePicker';
-import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import MovieCreationOutlinedIcon from '@mui/icons-material/MovieCreationOutlined';
+import { styled } from "@mui/material/styles";   
+import Button, { ButtonProps } from '@mui/material/Button';
 
 export const FilterByYear: FunctionComponent = () => {
-  const [startYear, setStartYear] = useState(new Date());
-  const [endYear, setEndYear] = useState(new Date());
+  const [startYear, setStartYear] = useState<Date | null>(new Date());
+  const [endYear, setEndYear] = useState<Date | null>(new Date());
 
   function convertUnixDateToDate(unixNumber: number) {
     const date = new Date(unixNumber * 1000);
@@ -20,32 +22,48 @@ export const FilterByYear: FunctionComponent = () => {
     const unixTimeStamp = date.getTime() / 1000;
     return unixTimeStamp;
   }
+  
+  const FilterButton = styled(Button)<ButtonProps>(({ theme }) => ({
+    backgroundColor: '#fff',
+    color: '#000',
+    '&:hover': {
+      backgroundColor: '#cccccc',
+      color:'#000'
+    },
+  }));
 
   console.log(convertUnixDateToDate(1551830400));
   console.log(convertDateToUnixDate(new Date(1551830400)));
 
   return (
-    <div>
-      <DatePicker
-        views={['year']}
-        label='From year'
-        value={startYear}
-        onChange={(newValue) => {
-          setStartYear(newValue);
-        }}
-        renderInput={(params) => <TextField {...params} helperText={null} />}
-      />
-      <DatePicker
-        views={['year']}
-        label='From year'
-        value={endYear}
-        onChange={(newValue) => {
-          setEndYear(newValue);
-        }}
-        renderInput={(params) => <TextField {...params} helperText={null} />}
-      />
-      
-
-    </div>
+    <>
+      <Box sx={{display:'flex'}}>
+        <LocalizationProvider dateAdapter={AdapterDateFns} > 
+          <DatePicker
+            views={['year']}
+            label='From year'
+            value={startYear}
+            onChange={(newValue) => {
+              setStartYear(newValue);
+            }}
+            renderInput={(params) => <TextField {...params} helperText={null} />}
+            className='date-picker-class'
+          />
+          <DatePicker
+            views={['year']}
+            label='To year'
+            value={endYear}
+            onChange={(newValue) => {
+              setEndYear(newValue);
+            }}
+            renderInput={(params) => <TextField {...params} helperText={null} />}
+            className='date-picker-class'
+          />
+        </LocalizationProvider>
+      </Box>
+      <div className="button-container">
+        <FilterButton variant="contained" endIcon={<MovieCreationOutlinedIcon/>}>Filter</FilterButton>
+      </div>
+    </>
   );
 };
