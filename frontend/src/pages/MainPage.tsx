e7037f260963b6d94bb6be11898185a7a8ed5b18
Dherik <dherikjd@stud.ntnu.no>
2021-10-24 01:29:37 +0200

    (#14) Set up draft for infinite scroll

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -4,32 +4,40 @@ import MovieService from "../services/index";
 import NavBar from "../components/navbar";
 import SideBar from "../components/sidebar/SideBar";
 import { Dispatch } from "redux";
-import { setMovies } from "./mainPageSlice"
+import { setMovies, setLoading } from "./mainPageSlice"
 import { getAllMovies, getAllMovies_getAllMovies } from "../services/__generated__/getAllMovies"
 import { useAppDispatch } from "../services/hooks"
 import { Layout } from 'antd';
 import { Row, Col, Nav } from "react-bootstrap";
 import { MovieTable } from "../components/movies";
+import Loader from "../components/loader/index"
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const { Header, Content, Sider } = Layout;
 
 const actionDispatch = (dispatch: Dispatch) => ({
-  setMovies: (movies: getAllMovies["getAllMovies"]) => dispatch(setMovies(movies))
+  setMovies: (movies: getAllMovies["getAllMovies"]) => dispatch(setMovies(movies)),
+  setLoading: (loading: boolean) => dispatch(setLoading(loading))
 });
 
 export const MainPage: FunctionComponent = () => {
   const { setMovies } = actionDispatch(useAppDispatch());
+  const { setLoading } = actionDispatch(useAppDispatch());
 
   const fetchMovies = async () => {
+    setLoading(true);
     const movies = await MovieService.getMoviesBySearch("", "Action", 1).catch((error) => {
       console.log("Error", error);
     });
 
     if(movies) {
       console.log("setting movies")
+      setLoading(false);
       setMovies(movies);
     }
   }
+
+  const hasMore = true
 
   useEffect(() => {
     fetchMovies();
@@ -47,7 +55,18 @@ export const MainPage: FunctionComponent = () => {
       <div className ="innercontainer">
         <SideBar />
         <div className="moviecontainer">
-          <MovieTable/>
+          <InfiniteScroll
+              dataLength={500}
+              next={fetchMovies}
+              hasMore={hasMore}
+              loader={<Loader />}
+              style={{
+                height: '100%',
+                overflow: 'visible',
+              }}
+            >
+              <MovieTable/>
+          </InfiniteScroll>
         </div>
       </div>
     </>
