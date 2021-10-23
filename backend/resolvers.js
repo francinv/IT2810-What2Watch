b80d99d247ff15dadcd9780fba5b3d1e64da8a5c
Dherik <dherikjd@stud.ntnu.no>
2021-10-23 15:27:11 +0200

    (#15) Added getMovieByPage query on backend

--- backend/resolvers.js
+++ backend/resolvers.js
@@ -2,21 +2,36 @@ const Movie = require('./models/Movie.model')
 const resolvers = {
     Query: {
         getAllMovies: async () => {
-            const movies = await Movie.find()
-            return movies
+            return await Movie.find()
+        },
+        getMoviePage: async (_parent, {page}, _context, _info) => {
+            const limit = 15
+            const offset = limit * (page - 1)
+            return (await Movie.find({})
+                .limit(parseInt(limit))
+                .skip(parseInt(offset)))
         },
         getMovieById: async (_parent, { id }, _context, _info) => {
             return await Movie.findById(id);
         },
-        getMoviesBySearch: async (_parent, args, context, _info) => {
+        getMoviesBySearch: async (_parent, args, _context, _info) => {
+            const limit = 15
+            const offset = limit * (args.page - 1)
             if (args.searchQuery && args.searchGenre) {
-                return await Movie.find({ genres: String(args.searchGenre), title: { $regex: String(args.searchQuery), $options: "i" } });
+                return await (Movie.find({ genres: String(args.searchGenre), title: { $regex: String(args.searchQuery), $options: "i" } })
+                .limit(parseInt(limit))
+                .skip(parseInt(offset)))
             }
             else if (!args.searchQuery && args.searchGenre) {
-                return await Movie.find({ genres: String(args.searchGenre) });
+                return await (Movie.find({ genres: String(args.searchGenre) })
+                .limit(parseInt(limit))
+                .skip(parseInt(offset)))
+                    
             }
             else if (args.searchQuery && !args.searchGenre) {
-                return await Movie.find({ title: { $regex: String(args.searchQuery), $options: "i" } });
+                return await (Movie.find({ title: { $regex: String(args.searchQuery), $options: "i" } })
+                .limit(parseInt(limit))
+                .skip(parseInt(offset)))
             }
         },
     },
