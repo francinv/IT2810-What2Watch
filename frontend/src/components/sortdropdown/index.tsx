f9f2924222ece24df5c2a39fa9be8d9aa0843167
francinv <francinv@stud.ntnu.no>
2021-10-27 00:45:18 +0200

    (#25) Created dropdown component

--- frontend/src/components/sortdropdown/index.tsx
+++ frontend/src/components/sortdropdown/index.tsx
@@ -4,29 +4,45 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
+import './index.css';
 
 export default function SortDropDown() {
-  const [sorting, setSorting] = React.useState('');
+  const [yearssort, setYearsSort] = React.useState('');
+  const [titlesort, setTitleSort] = React.useState('');
 
-  const handleChange = (event: SelectChangeEvent) => {
-    setSorting(event.target.value as string);
+  const handleYears = (event: SelectChangeEvent) => {
+    setYearsSort(event.target.value as string);
   };
 
+  const handleTitle = (event: SelectChangeEvent) => {
+    setTitleSort(event.target.value as string);
+  }
+
   return (
-    <Box sx={{ minWidth: 120 }}>
+    <Box className="select-container" sx={{ display:'flex', minWidth:300 }}>
       <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Sort</InputLabel>
+        <InputLabel id="demo-simple-select-label">Year</InputLabel>
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
-          value={sorting}
-          label="Sort"
-          onChange={handleChange}
+          value={yearssort}
+          label="Year"
+          onChange={handleYears}
         >
-          <MenuItem value={"yincreasing"}>Years Increasing</MenuItem>
-          <MenuItem value={"ydecreasing"}>Years Decreasing</MenuItem>
-          <MenuItem value={"tascending"}>Title Ascending</MenuItem>
-          <MenuItem value={"tdescending"}>Title Descending</MenuItem>
+          <MenuItem value={"yincreasing"}>Increasing</MenuItem>
+          <MenuItem value={"ydecreasing"}>Decreasing</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Title</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={titlesort}
+          label="Title"
+          onChange={handleTitle}
+        >
+          <MenuItem value={"yincreasing"}>Alphabetical</MenuItem>
         </Select>
       </FormControl>
     </Box>
