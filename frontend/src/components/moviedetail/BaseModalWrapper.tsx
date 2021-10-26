e9d4b79ed26ff57a2e2ead56e917359ec92f436a
francinv <francinv@stud.ntnu.no>
2021-10-26 22:47:09 +0200

    (#16) Created Wrapper for Modal

---
+++ frontend/src/components/moviedetail/BaseModalWrapper.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Modal from './Modal';
+
+interface BaseModalWrapperProps {
+    isModalVisible: boolean;
+    onBackDropClick: () => void;
+    movies: [];
+}
+
+const BaseModalWrapper: React.FC<BaseModalWrapperProps> = ({onBackDropClick, isModalVisible}) => {
+    if (isModalVisible){
+        return null;
+    }
+    return(<Modal onBackDropClick={onBackDropClick}/>);
+}
+
+export default BaseModalWrapper;
