ff609cb51b5465666e8d14ea513f73fd1aae1ab6
francinv <francinv@stud.ntnu.no>
2021-10-27 23:11:28 +0200

    (#16) Updated arguments and some method for closing modal

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -66,6 +66,17 @@ export const MainPage: FunctionComponent = () => {
     }
   }
 
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const toggleModal = () => {
+    setIsModalVisible(wasModalVisible => !wasModalVisible);
+  }
+
+  const closeModal = () => {
+    if(isModalVisible){
+      setIsModalVisible(false);
+    }
+  }
+
   return (
     <>
       <Row>
