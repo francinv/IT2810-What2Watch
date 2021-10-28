825f990f8e259e8334191324b0c2b363624a2edb
kavusi98 <kavusikan1998@gmail.com>
2021-10-28 14:39:10 +0200

    (#21) implementet sorting in backend

--- backend/resolvers.ts
+++ backend/resolvers.ts
@@ -1,32 +1,38 @@
-import Movie from './models/Movie.model';
+import Movie from "./models/Movie.model";
 
 const resolvers = {
-    Query: {
-        getMoviesBySearch: async (_parent: unknown, args: {
-            searchDateStart: number,
-            searchDateEnd: number,
-            searchQuery: string,
-            searchGenre: string[],
-            page: number
-        }) => {
-            const limit = 30
-            const offset = limit * (args.page)
-                return await (Movie.find({
-                    title: {
-                        $regex: args.searchQuery,
-                        $options: "i" 
-                    } ,
-                    release_date: {
-                        $gte: args.searchDateStart,
-                        $lte: args.searchDateEnd
-                    },
-                    genres: {
-                        $all: Array.from(args.searchGenre) 
-                }})
-                .limit(limit)
-                .skip(offset))
+  Query: {
+    getMoviesBySearch: async (
+      _parent: unknown,
+      args: {
+        searchDateStart: number;
+        searchDateEnd: number;
+        searchQuery: string;
+        searchGenre: string[];
+        page: number;
+        sortCriteria: string;
+      }
+    ) => {
+      const limit = 30;
+      const offset = limit * args.page;
+      return await Movie.find({
+        title: {
+          $regex: args.searchQuery,
+          $options: "i",
         },
+        release_date: {
+          $gte: args.searchDateStart,
+          $lte: args.searchDateEnd,
+        },
+        genres: {
+          $all: Array.from(args.searchGenre),
+        },
+      })
+        .limit(limit)
+        .skip(offset)
+        .sort(args.sortCriteria);
     },
+  },
 };
 
 module.exports = resolvers;
--- backend/typeDefs.ts
+++ backend/typeDefs.ts
@@ -1,19 +1,25 @@
-import { gql } from 'apollo-server-express';
+import { gql } from "apollo-server-express";
 
 const typeDefs = gql`
+  type Movie {
+    id: ID
+    title: String
+    release_date: Float
+    genres: [String]
+    overview: String
+    poster: String
+  }
 
-    type Movie {
-        id:ID
-        title: String
-        release_date: Float
-        genres: [String]
-        overview: String
-        poster: String
-    }
-
-    type Query {
-        getMoviesBySearch(searchDateStart: Int, searchDateEnd: Int, searchQuery: String, searchGenre: [String], page: Int): [Movie]
-    }
+  type Query {
+    getMoviesBySearch(
+      searchDateStart: Int
+      searchDateEnd: Int
+      searchQuery: String
+      searchGenre: [String]
+      page: Int
+      sortCriteria: String
+    ): [Movie]
+  }
 `;
 
-module.exports = typeDefs
+module.exports = typeDefs;
