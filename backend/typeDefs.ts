c14ef84bde1de76c99ecee1ef6a5158ed3418f97
Dherik <dherikjd@stud.ntnu.no>
2021-10-28 17:23:20 +0200

    (#30) Added mutation to resolver

--- backend/typeDefs.ts
+++ backend/typeDefs.ts
@@ -20,6 +20,28 @@ const typeDefs = gql`
       sortCriteria: String
     ): [Movie]
   }
+    type Movie {
+        id:ID
+        title: String
+        release_date: Float
+        genres: [String]
+        overview: String
+        poster: String
+    }
+
+    type User { 
+        id: ID
+        name: String
+        favorites: [Movie]
+    }
+
+    type Query {
+        getMoviesBySearch(searchDateStart: Int, searchDateEnd: Int, searchQuery: String, searchGenre: [String], page: Int): [Movie]
+    }
+
+    type Mutation { 
+        setMovieAsFavorite(name: String, movie: String): String
+    }
 `;
 
 module.exports = typeDefs;
