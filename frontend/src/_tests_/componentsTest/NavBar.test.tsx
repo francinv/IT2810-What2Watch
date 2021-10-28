01cd4cff631cbbbd962b0127e1efcaf8fdeefc56
kavusi98 <kavusikan1998@gmail.com>
2021-10-29 01:18:32 +0200

    (#18) Added component test for navbar

---
+++ frontend/src/_tests_/componentsTest/NavBar.test.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {render, screen} from "../test-utils"
+import userEvent from "@testing-library/user-event"
+import NavBar from "../../components/navbar";
+
+describe("NavBar", ()=> {
+    test("navbar is showing logo text", () => {
+        render(<NavBar/>)
+        expect(screen.getByText("What to Watch?")).toBeInTheDocument();
+    })
+    test("search field is in navbar", () => {
+        const {getByLabelText} = render(<NavBar/>)
+        expect(getByLabelText(/search/i)).toBeInTheDocument();
+    })
+    test("search in navbar is typeable", () => {
+        const {getByLabelText} = render(<NavBar/>)
+        const searchField= getByLabelText(/search/i)
+        userEvent.type(searchField, "hei");
+        expect(searchField).toHaveValue("hei")
+    })
+})
