67538c338472ae551a2248aa901a2e37e828f0b3
kavusi98 <kavusikan1998@gmail.com>
2021-10-28 16:54:12 +0200

    (#21) Updated sorting options

--- frontend/src/components/sortdropdown/index.tsx
+++ frontend/src/components/sortdropdown/index.tsx
@@ -1,14 +1,15 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import './index.css';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import "./index.css";
+import { SortBy } from "../../util/sortingTypes";
 
 export default function SortDropDown() {
-  const [yearssort, setYearsSort] = React.useState('');
-  const [titlesort, setTitleSort] = React.useState('');
+  const [yearssort, setYearsSort] = React.useState("");
+  const [titlesort, setTitleSort] = React.useState("");
 
   const handleYears = (event: SelectChangeEvent) => {
     setYearsSort(event.target.value as string);
@@ -16,12 +17,12 @@ export default function SortDropDown() {
 
   const handleTitle = (event: SelectChangeEvent) => {
     setTitleSort(event.target.value as string);
-  }
+  };
 
   return (
-    <Box className="select-container" sx={{ display:'flex', minWidth:300 }}>
+    <Box className="select-container" sx={{ display: "flex", minWidth: 300 }}>
       <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Year</InputLabel>
+        <InputLabel id="demo-simple-select-label">Sort by</InputLabel>
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
@@ -29,20 +30,13 @@ export default function SortDropDown() {
           label="Year"
           onChange={handleYears}
         >
-          <MenuItem value={"yincreasing"}>Increasing</MenuItem>
-          <MenuItem value={"ydecreasing"}>Decreasing</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Title</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={titlesort}
-          label="Title"
-          onChange={handleTitle}
-        >
-          <MenuItem value={"yincreasing"}>Alphabetical</MenuItem>
+          <MenuItem value={SortBy.AlphabeticalAsc}>Title (Increasing)</MenuItem>
+          <MenuItem value={SortBy.AlphabeticalDesc}>
+            Title (Decreasing)
+          </MenuItem>
+          <MenuItem value={SortBy.YearAsc}>Release Year (Increasing)</MenuItem>
+          <MenuItem value={SortBy.YearDesc}>Release Year (Decreasing)</MenuItem>
+          <MenuItem value={SortBy.Clear}>Clear</MenuItem>
         </Select>
       </FormControl>
     </Box>
