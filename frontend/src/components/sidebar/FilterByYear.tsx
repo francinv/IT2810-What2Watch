849f5ae2a071005b0e8191457552967803120bd7
kavusi98 <kavusikan1998@gmail.com>
2021-10-28 02:09:15 +0200

    (#21) small date converter fix

--- frontend/src/components/sidebar/FilterByYear.tsx
+++ frontend/src/components/sidebar/FilterByYear.tsx
@@ -1,85 +1,96 @@
 import { FunctionComponent, useState } from "react";
-import TextField from '@mui/material/TextField';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import DatePicker from '@mui/lab/DatePicker';
-import Box from '@mui/material/Box';
-import MovieCreationOutlinedIcon from '@mui/icons-material/MovieCreationOutlined';
-import { styled } from "@mui/material/styles";   
-import Button, { ButtonProps } from '@mui/material/Button';
-import { useAppDispatch } from "../../services/hooks"
+import TextField from "@mui/material/TextField";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import DatePicker from "@mui/lab/DatePicker";
+import Box from "@mui/material/Box";
+import MovieCreationOutlinedIcon from "@mui/icons-material/MovieCreationOutlined";
+import { styled } from "@mui/material/styles";
+import Button, { ButtonProps } from "@mui/material/Button";
+import { useAppDispatch } from "../../services/hooks";
 import { Dispatch } from "redux";
-import { setFilterDates } from "../../pages/mainPageSlice"
+import { setFilterDates } from "../../pages/mainPageSlice";
+import "./index.css";
 
 const actionDispatch = (dispatch: Dispatch) => ({
   setDates: (year: [number, number]) => dispatch(setFilterDates(year)),
 });
 
-export const FilterByYear: FunctionComponent = () => { 
+export const FilterByYear: FunctionComponent = () => {
+  const [startDate, setStateStartDate] = useState<number>(1635203598);
+  const [endDate, setStateEndDate] = useState<number>(1635203598);
 
-  const [startDate, setStateStartDate] = useState<number>(1635203598)
-  const [endDate, setStateEndDate] = useState<number>(1635203598)
-
-  const { setDates } = actionDispatch(useAppDispatch())
+  const { setDates } = actionDispatch(useAppDispatch());
 
   function convertDateToUnixDate(date: Date) {
-    return date.getTime() / 1000;
+    return Math.round(date.getTime() / 1000);
   }
-  
+
   const FilterButton = styled(Button)<ButtonProps>(({ theme }) => ({
-    backgroundColor: '#fff',
-    color: '#000',
-    '&:hover': {
-      backgroundColor: '#cccccc',
-      color:'#000'
+    backgroundColor: "#fff",
+    color: "#000",
+    "&:hover": {
+      backgroundColor: "#cccccc",
+      color: "#000",
     },
   }));
 
   function setStartYear(year: number | null) {
     if (year !== null) {
-      setStateStartDate(convertDateToUnixDate(new Date(year)))
+      setStateStartDate(convertDateToUnixDate(new Date(year)));
     }
-    
   }
   function setEndYear(year: number | null) {
-    if (year !== null) {setStateEndDate(convertDateToUnixDate(new Date(year)));}
+    if (year !== null) {
+      setStateEndDate(convertDateToUnixDate(new Date(year)));
+    }
   }
 
   function setFilters() {
     if (endDate > startDate) {
-      setDates([startDate, endDate])
+      setDates([startDate, endDate]);
+      console.log("HJ", startDate, endDate);
     }
   }
 
-
   return (
     <>
-      <Box sx={{display:'flex'}}>
-        <LocalizationProvider dateAdapter={AdapterDateFns} > 
+      <Box sx={{ display: "flex" }}>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
           <DatePicker
-            views={['year']}
-            label='From'
+            views={["year"]}
+            label="From"
             value={null}
             onChange={(newValue) => {
               setStartYear(newValue);
             }}
-            renderInput={(params) => <TextField {...params} helperText={null} />}
-            className='date-picker-class'
+            renderInput={(params) => (
+              <TextField {...params} helperText={null} />
+            )}
+            className="date-picker-class"
           />
           <DatePicker
-            views={['year']}
-            label='To'
+            views={["year"]}
+            label="To"
             value={null}
             onChange={(newValue) => {
               setEndYear(newValue);
             }}
-            renderInput={(params) => <TextField {...params} helperText={null} />}
-            className='date-picker-class'
+            renderInput={(params) => (
+              <TextField {...params} helperText={null} />
+            )}
+            className="date-picker-class"
           />
         </LocalizationProvider>
       </Box>
       <div className="button-container">
-        <FilterButton variant="contained" endIcon={<MovieCreationOutlinedIcon/>} onClick={() => setFilters()}>Filter</FilterButton>
+        <FilterButton
+          variant="contained"
+          endIcon={<MovieCreationOutlinedIcon />}
+          onClick={() => setFilters()}
+        >
+          Filter
+        </FilterButton>
       </div>
     </>
   );
