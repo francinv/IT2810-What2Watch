aca472011eff8aa1aa86a198066c149c34c5806e
kavusi98 <kavusikan1998@gmail.com>
2021-10-29 02:04:07 +0200

    (#18) Added component test for SideBar

--- frontend/src/_tests_/test-utils.tsx
+++ frontend/src/_tests_/test-utils.tsx
@@ -3,7 +3,6 @@ import React, { ReactElement } from 'react'
 import { render as rtlRender, RenderOptions } from '@testing-library/react'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
-// Import your own reducer
 import mainPageReducer from "../pages/mainPageSlice";
 import { Router } from '@mui/icons-material';
 import { store } from '../services/store';
