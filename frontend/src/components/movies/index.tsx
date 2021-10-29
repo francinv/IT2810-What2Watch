bbf86a4cfd845ac521686aca92ab97b5ce6043af
kavusi98 <kavusikan1998@gmail.com>
2021-10-29 12:09:19 +0200

    (#26) Made sidebar sticky

--- frontend/src/components/movies/index.tsx
+++ frontend/src/components/movies/index.tsx
@@ -13,7 +13,7 @@ import { useState } from "react";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import IconButton from "@mui/material/IconButton";
-import BaseModalWrapper from "./BaseModalWrapper";
+import BaseModalWrapper from "../moviedetail/BaseModalWrapper";
 import {
   formatDateAsString,
   convertUnixDateToDate,
