f17fa4ada88dde5e608d500c24e95d50eb5a90aa
kavusi98 <kavusikan1998@gmail.com>
2021-10-30 14:44:46 +0200

    (#18) Added component testing for SortDropDown

---
+++ frontend/src/_tests_/componentsTest/SortDropDownTest.test.tsx
@@ -0,0 +1,21 @@
+import {render, screen} from "../test-utils"
+import userEvent from "@testing-library/user-event"
+import SortDropDown from "../../components/sortdropdown";
+
+describe("SortDropDown", ()=> {
+    test("sortdropdown is showing", () => {
+        render(<SortDropDown/>)
+        expect(screen.getByText("Sort by")).toBeInTheDocument();
+    })
+    test("Sortdropdown includes correct options", () => {
+        const {getByLabelText, getByText} = render(<SortDropDown/>)
+        const dropDown = screen.getByLabelText(/Sort by/)
+        userEvent.click(dropDown)
+        expect(getByText("None")).toBeInTheDocument();
+        expect(getByText("Title (Increasing)")).toBeInTheDocument();
+        expect(getByText("Title (Decreasing)")).toBeInTheDocument();
+        expect(getByText("Release Year (Increasing)")).toBeInTheDocument();
+        expect(getByText("Release Year (Decreasing)")).toBeInTheDocument();
+    })
+    
+})
