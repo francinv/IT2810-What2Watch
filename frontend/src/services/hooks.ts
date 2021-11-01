064ec180d367307f738139f44761e54e8b0b9954
Dherik <dherikjd@stud.ntnu.no>
2021-11-01 01:10:01 +0100

    (#19) Commented files in services folder

--- frontend/src/services/hooks.ts
+++ frontend/src/services/hooks.ts
@@ -2,5 +2,7 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { RootState, AppDispatch } from "./store";
 
+/* Redux hooks to use dispatch */
+
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
